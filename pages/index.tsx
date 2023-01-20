232d53e448cf65ee0dcfb23548835bd646c799cc
acevedonm <acevedonm@gmail.com>
2023-01-20 02:01:18 -0300

    table

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,14 @@ import { useAppContext } from "../context/DataContext";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { data, Expedientes } from "../utils/data";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -20,6 +27,16 @@ import useDownloader from "react-use-downloader";
 import { styled } from "@mui/material/styles";
 import { FilesService } from "../utils/files.service";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
+
+function createData(
+  iniciador: string,
+  numero: string,
+  anio: string,
+  prefijo: string,
+  extension: string
+): Expedientes {
+  return { iniciador, numero, anio, prefijo, extension };
+}
 
 function generate(element: React.ReactElement) {
   return [0].map((value) =>
@@ -40,6 +57,17 @@ const Demo = styled("div")(({ theme }) => ({
 
 export default function Home() {
   const [encontrado, setEncontrado] = React.useState(false);
+
+  const inicialRows = data.map((element) =>
+    createData(element.iniciador, element.numero, element.anio, "4069", "24")
+  );
+
+  console.log({ inicialRows });
+  const [rows, setRows] = React.useState(inicialRows);
+  const [fieldsSearch, setFieldsSearch] = React.useState({
+    exp: "",
+    year: "",
+  });
   const [loading, setLoading] = React.useState(false);
   const redirect = () => {
     setLoading(true);
@@ -48,9 +76,36 @@ export default function Home() {
     }, 2000);
   };
 
-  const buscar = () => {
-    console.log("buscando");
+  const verTodos = () => {
+    //seteo nuevas rows setRows
+    const newData = data;
+    setRows(newData);
     setEncontrado(true);
+  };
+
+  const buscar = () => {
+    //seteo nuevas rows setRows
+    const newData = data.filter(
+      (element) =>
+        fieldsSearch.exp == element.numero || fieldsSearch.year == element.anio
+    );
+    console.log;
+    setRows(newData);
+    setEncontrado(true);
+    console.log({ rows });
+  };
+
+  const changeSeachYear = (event) => {
+    setFieldsSearch({
+      ...fieldsSearch,
+      year: event.target.value,
+    });
+  };
+  const changeSeachExp = (event) => {
+    setFieldsSearch({
+      ...fieldsSearch,
+      exp: event.target.value,
+    });
   };
   const [dense, setDense] = React.useState(false);
   const [secondary, setSecondary] = React.useState(false);
@@ -77,18 +132,67 @@ export default function Home() {
               defaultValue="4069"
               type="search"
             />
-            <TextField id="outlined-search" label="N°" type="search" />
+            <TextField
+              id="outlined-search"
+              label="N°"
+              type="search"
+              onChange={changeSeachExp}
+            />
           </div>
           <div>
-            <TextField id="outlined-search" label="Año" type="search" />
+            <TextField
+              id="outlined-search"
+              label="Año"
+              type="search"
+              onChange={changeSeachYear}
+            />
             <TextField id="outlined-search" label="Extension" type="search" />
           </div>
-          <Button variant="contained" onClick={buscar}>
+          <Button
+            variant="contained"
+            onClick={buscar}
+            style={{ marginRight: "10px" }}
+          >
             Buscar
+          </Button>
+          <Button variant="contained" onClick={verTodos}>
+            Ver Todos
           </Button>
         </Box>
         {encontrado ? (
           <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Iniciador</TableCell>
+                    <TableCell align="right">N° Expediente</TableCell>
+                    <TableCell align="right">Año </TableCell>{" "}
+                    <TableCell align="right">Prefijo </TableCell>
+                    <TableCell align="right">Extensión </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.iniciador}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.iniciador}
+                      </TableCell>
+                      <TableCell align="right">{row.numero}</TableCell>
+                      <TableCell align="right">{row.anio}</TableCell>
+                      <TableCell align="right">{row.prefijo}</TableCell>
+                      <TableCell align="right">{row.extension}</TableCell>
+                      <TableCell align="right">
+                        <Button>Descargar</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
             <Grid item xs={12} md={6}>
               <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
                 Resultados
---
+++ utils/data.ts
@@ -0,0 +1,80 @@
+export class Expedientes {
+  iniciador: string;
+  numero: string;
+  anio: string;
+  prefijo: string;
+  extension: string;
+}
+
+export const data: Expedientes[] = [
+  {
+    iniciador: "-",
+    numero: "1146",
+    anio: "2021",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "1217",
+    anio: "2021",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "1956",
+    anio: "2014",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "2766",
+    anio: "2017",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "3094",
+    anio: "2021",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "3310",
+    anio: "2017",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "4834",
+    anio: "2020",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "6237",
+    anio: "2019",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "7406",
+    anio: "2019",
+    prefijo: "4069",
+    extension: "-",
+  },
+  {
+    iniciador: "-",
+    numero: "9675",
+    anio: "2021",
+    prefijo: "4069",
+    extension: "-",
+  },
+];
