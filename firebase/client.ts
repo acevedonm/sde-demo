b23cec78f0db8796f2b66fe3e1d1c1f5db181582
acevedonm <acevedonm@gmail.com>
2023-01-19 22:05:47 -0300

    searcher

---
+++ firebase/client.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVWmoWfGz7pQXiWWCQTM11RTc_d89Y1dc",
+  authDomain: "sde-demo-fca0f.firebaseapp.com",
+  projectId: "sde-demo-fca0f",
+  storageBucket: "sde-demo-fca0f.appspot.com",
+  messagingSenderId: "544602591545",
+  appId: "1:544602591545:web:faa68860c8d58df9a558e8",
+  measurementId: "G-2JYL1NKVYJ",
+};
+
+firebase.initializeApp(firebaseConfig);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { Container } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
+import * as React from "react";
 import { useAppContext } from "../context/DataContext";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-import * as React from "react";
+
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -19,6 +19,7 @@ import FolderIcon from "@mui/icons-material/Folder";
 import useDownloader from "react-use-downloader";
 import { styled } from "@mui/material/styles";
 import { FilesService } from "../utils/files.service";
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 function generate(element: React.ReactElement) {
   return [0].map((value) =>
@@ -38,7 +39,8 @@ const Demo = styled("div")(({ theme }) => ({
 }));
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
+  const [encontrado, setEncontrado] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const redirect = () => {
     setLoading(true);
     setTimeout(function () {
@@ -46,59 +48,78 @@ export default function Home() {
     }, 2000);
   };
 
+  const buscar = () => {
+    console.log("buscando");
+    setEncontrado(true);
+  };
   const [dense, setDense] = React.useState(false);
   const [secondary, setSecondary] = React.useState(false);
 
   return (
     /*  Aca va el login */
-    <Container>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-          <TextField id="outlined-search" label="Iniciador" type="search" />
-          <TextField id="outlined-search" label="Prefijo" type="search" />
-          <TextField id="outlined-search" label="N°" type="search" />
-        </div>
-        <div>
-          <TextField id="outlined-search" label="Año" type="search" />
-          <TextField id="outlined-search" label="Extension" type="search" />
-        </div>
-        <Button variant="contained" onClick={() => download()}>
-          Buscar
-        </Button>
-      </Box>
-      <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Resultados
-          </Typography>
-          <a download="prueba.pdf" href="assets/constanciaCBU.pdf">
-            {" "}
-            DESCARGAR A
-          </a>
-          <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem>
-                  <ListItemIcon>
-                    <FolderIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary="Exp. 1111/2015"
-                    secondary={secondary ? "Secondary text" : null}
-                  />
-                </ListItem>
-              )}
-            </List>
-          </Demo>
-        </Grid>
-      </Box>
-    </Container>
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Container>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField id="outlined-search" label="Iniciador" type="search" />
+            <TextField
+              disabled
+              id="outlined-search"
+              label="Prefijo"
+              defaultValue="4069"
+              type="search"
+            />
+            <TextField id="outlined-search" label="N°" type="search" />
+          </div>
+          <div>
+            <TextField id="outlined-search" label="Año" type="search" />
+            <TextField id="outlined-search" label="Extension" type="search" />
+          </div>
+          <Button variant="contained" onClick={buscar}>
+            Buscar
+          </Button>
+        </Box>
+        {encontrado ? (
+          <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <Grid item xs={12} md={6}>
+              <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                Resultados
+              </Typography>
+              <a
+                href="/assets/constanciaCBU.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                download="prueba.pdf"
+              >
+                Download FIle
+              </a>
+              <Demo>
+                <List dense={dense}>
+                  {generate(
+                    <ListItem>
+                      <ListItemIcon>
+                        <FolderIcon />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary="Exp. 1111/2015"
+                        secondary={secondary ? "Secondary text" : null}
+                      />
+                    </ListItem>
+                  )}
+                </List>
+              </Demo>
+            </Grid>
+          </Box>
+        ) : null}
+      </Container>
+    </>
   );
 }
