516cfdbb1117f6f77110a66e63a88045b1e44b5f
acevedonm <acevedonm@gmail.com>
2023-06-26 02:44:40 -0300

    commit

--- firebase/getAllExp.ts
+++ firebase/getAllExp.ts
@@ -8,6 +8,8 @@ import {
   startAfter,
 } from "firebase/firestore";
 import firebaseApp from "./client";
+import PaginatedResponse from "../src/interfaces/paginatedResponse";
+import { Expedientes } from "../src/interfaces/expedientes";
 
 
 const db = getFirestore(firebaseApp);
@@ -32,11 +34,21 @@ export async function getAllExp () {
 }
 
 
+/**
+* En la función getExpedientesPorPagina, recibes pageNumber como parámetro, que indica el número de página que deseas obtener. Luego, estableces pageSize para determinar la cantidad de documentos por página.
+* La variable startAfterDocument se calcula para determinar qué documento debe ser utilizado como referencia con startAfter() en la consulta. Si pageNumber es mayor que 1, se calcula el índice del documento en la página anterior. Si pageNumber es 1, se establece en null porque no se necesita startAfter() para la primera página.
+* Luego, se crea la consulta utilizando query() y limit() para obtener los documentos correspondientes a la página actual. Si startAfterDocument no es null, se obtiene la referencia al documento correspondiente utilizando la función getDocumentReference().
+* Finalmente, se obtiene el QuerySnapshot utilizando getDocs(), se extraen los documentos en pageDocuments y se devuelve un arreglo de objetos que contienen el id del documento y los datos del mismo.
+* La función getDocumentReference se utiliza para obtener la referencia al documento en base al índice proporcionado. Esto se logra obteniendo todos los documentos de la colección y seleccionando el documento en el índice específico. 
+* @param {number} pageNumber - numero de paginas
+*/
 export async function getExpedientesPorPagina(pageNumber: number = 0, pageSize: number = 10) {
   const startAfterDocument = pageNumber > 1 ? (pageNumber - 1) * pageSize : null;
 
-  console.log({startAfterDocument})
+  console.log({ startAfterDocument });
+
   let pageQuery = query(collection(db, "expedientes"), limit(pageSize));
+
   if (startAfterDocument) {
     const startAfterDocRef = await getDocumentReference(startAfterDocument);
     pageQuery = query(pageQuery, startAfter(startAfterDocRef));
@@ -45,11 +57,18 @@ export async function getExpedientesPorPagina(pageNumber: number = 0, pageSize: 
   const pageSnapshot = await getDocs(pageQuery);
   const pageDocuments = pageSnapshot.docs;
 
-  return pageDocuments.map((doc) => ({
+  const totalResults = pageSnapshot.size;
+  const totalPages = Math.ceil(totalResults / pageSize);
+
+  const expedientes = pageDocuments.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   }));
+
+
+  return expedientes;
 }
+
 
 
 async function getDocumentReference(documentIndex) {
---
+++ src/interfaces/paginatedResponse.ts
@@ -0,0 +1,16 @@
+
+
+export default interface PaginatedResponse<T> {
+  error: {};
+  metadata: {
+    pagination: PaginationNode;
+  };
+  data: T;
+}
+
+interface PaginationNode {
+  page: number;
+  pageResults: number;
+  totalPages: number;
+  totalResults: number;
+}
