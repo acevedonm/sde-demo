2800fea0025f01cb55dcd77153f5870f9d275cf2
Santiago Bernardi <108844968+SantiagoBernardi@users.noreply.github.com>
2024-07-18 19:27:55 -0300

    Feat/busqueda (#20)
    * fix: se modifica la carga de csv para que esta sea mas eficiente
    
    * Feat: ahora al presionar enter en la busqueda de expedientes, se lanza la busqueda

--- firebase/uploader-job-db.ts
+++ firebase/uploader-job-db.ts
@@ -6,12 +6,12 @@ export default async function uploaderJob(file, setLoading) {
   Papa.parse(file, {
     header: true,
     skipEmptyLines: true,
-    complete: function (results) {
+    complete: async function (results) {
       const dataByYear = {};
 
       // Organizar datos por año
       results.data.forEach((d) => {
-        const year = d.año; // Asegúrate de reemplazar 'año' con el nombre real de la columna del año
+        const year = d.year; // Asegúrate de reemplazar 'year' con el nombre real de la columna del año
         if (!dataByYear[year]) {
           dataByYear[year] = [];
         }
@@ -19,17 +19,9 @@ export default async function uploaderJob(file, setLoading) {
       });
 
       // Iterar sobre grupos por año y pasarlos a otra función
-      Object.keys(dataByYear).forEach((year) => {
-        console.log(`Procesando datos para el año ${year}`);
-        const dataForYear = dataByYear[year];
-        dataForYear.forEach((d, index) => {
-          console.log("Subiendo datos de expediente: ", d);
-          d.file = "";
-          uploadData(d);
-          console.log({ d });
-        });
-        console.log(`Registros guardados para el año ${year}: ${dataForYear.length}`);
-      });
+      for (const year of Object.keys(dataByYear)) {
+        await uploadData(dataByYear[year]);
+      }
 
       setLoading(false);
     },
